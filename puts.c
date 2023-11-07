523c1278c2ed43db07983561bece6b350850498b
Ayo <106088806+wagzyAyo@users.noreply.github.com>
2023-11-07 10:57:26 -0800

    Create puts.c
    puts

---
+++ puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+        char str[70];
+
+        strcpy(str, "Programming is like building a multilingual puzzle");
+
+        puts(str);
+
+        return(0);
+}
