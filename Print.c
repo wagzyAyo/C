a65ae857bafc5e1170b6b3b5222a143182f56e97
wagzyAyo <talktojmcvibes@gmail.com>
2023-11-24 02:17:16 -0800

    print

--- Print.c
+++ Print.c
@@ -1,5 +1,8 @@
-int main
+#include <stdio.h>
+
+int main()
 {
-  printf("with proper grammar, but the outcome is a piece of art,", %s);
-  return (0);
+	printf("with proper grammar, but the outcome is a piece of art\n");
+	
+	return (0);
 }
