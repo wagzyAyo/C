07c2ada3300c53fde6c9c96afb9a8a7129efe958
Ayo <talktojmcvibes@gmail.com>
2024-07-31 11:53:20 +0100

    Continue calculating on loop

--- Calculator/main.c
+++ Calculator/main.c
@@ -1,30 +1,38 @@
 #include <stdio.h>
 
+int calculator(int num1, int num2, char operaration){
+    if (operaration == '+'){
+        return num1 + num2;
+    } else if (operaration == '-'){
+        return num1 - num2;
+    } else if (operaration == '*'){
+        return  num1 * num2;
+    } else if(operaration == '/'){
+        return num1/num2;
+    }else{
+        printf("Invalid operation");
+    }
+}
+
 int main(){
     int num1, num2;
     char opearation;
+    int continuety = 1;
 
-    printf("Enter first number: ");
+    while (continuety){
+        printf("\nEnter first number: ");
     scanf("%d", &num1);
     printf("Enter Operation to perform(+ - * /): ");
     scanf(" %c", &opearation);
     printf("Enter second number: ");
     scanf("%d", &num2);
 
-    int result = 0;
-    if (opearation == '+'){
-        result = num1 + num2;
-    } else if (opearation == '-'){
-        result = num1 - num2;
-    } else if (opearation == '*'){
-        result = num1 * num2;
-    } else if(opearation == '/'){
-        result = num1/num2;
-    }else{
-        printf("Invalid operation");
-    }
+    int result = calculator(num1, num2, opearation);
+    
 
     printf("The result is %d", result);
+    }
+    
 
     return 0;
 }
